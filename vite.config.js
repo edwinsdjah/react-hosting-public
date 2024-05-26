1061790e6c47e3b67504d7e64d94013602f38d94
Edwin Syah <edwinsdjah@gmail.com>
2024-05-26 14:01:55 +0700

    commit PWA

--- vite.config.js
+++ vite.config.js
@@ -1,7 +1,34 @@
 import { defineConfig } from 'vite'
 import react from '@vitejs/plugin-react'
+import { VitePWA } from "vite-plugin-pwa";
 
 // https://vitejs.dev/config/
 export default defineConfig({
-  plugins: [react()],
+  plugins: [
+    react(),
+    VitePWA({
+      manifest: {
+        name: 'React Tes',
+        short_name: 'React Tes App',
+        start_url: '/',
+        display: 'standalone',
+        theme_color: '#ffffff',
+        background_color: '#ffffff',
+        icons: [
+          {
+            src: 'icons/android-icon-192x192.png',
+            sizes: '192x192',
+            type: 'image/png'
+          }
+          // {
+          //   src: 'icon-512x512.png',
+          //   sizes: '512x512',
+          //   type: 'image/png'
+          // }
+        ]
+      }
+    })
+  ],
 })
+
+
